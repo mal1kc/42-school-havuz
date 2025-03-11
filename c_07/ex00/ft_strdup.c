d2f76b744a8e325dcee1c13a843bd0e7df2b82ff
Mustafa Malik Kokcan <mkokcan@k1m05s02.42kocaeli.com.tr>
2025-03-11 15:37:51 +0300

    update c_07 for evo

--- c_07/ex00/ft_strdup.c
+++ c_07/ex00/ft_strdup.c
@@ -6,30 +6,24 @@
 /*   By: mkokcan <mkokcan@student.42kocaeli.com.tr  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 11:22:20 by mkokcan           #+#    #+#             */
-/*   Updated: 2025/03/06 11:34:02 by mkokcan          ###   ########.fr       */
+/*   Updated: 2025/03/11 15:35:21 by mkokcan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 char	*ft_strdup(char	*src)
 {
-	unsigned int counter;
-	unsigned int writer;
-	char	*target;
-	
+	unsigned int	counter;
+	unsigned int	writer;
+	char			*target;
+
 	counter = 0;
 	writer = 0;
-	while(src[counter] != '\0')
-	{
+	while (src[counter] != '\0')
 		counter++;
-	}
-	
-	target =(char *) malloc(sizeof(char) * counter);
-	if(target == (NULL))
-	{
+	target = (char *)malloc(sizeof(char) * counter);
+	if (target == (NULL))
 		return (target);
-	}
-	while(writer < counter)
+	while (writer < counter)
 	{
 		target[writer] = src[writer];
 		writer++;
